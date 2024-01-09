e2d9a67ca3d7914d8577990adb8a4e0ee914d216
Paul Marini <paul@safeaccessnow.org>
2024-01-08 19:38:39 -0800

    handle query params

--- functions/nb_call.js
+++ functions/nb_call.js
@@ -4,11 +4,12 @@
  * @param {object} clientConfig - client-specific config info
  * @param {string} method - get, post, etc
  * @param {string} endpoint - partial url of the endpoint
- * @param {object=} data - data object, if needed for put and post endpoints
- * @param {number=} id - resource ID, if needed for get, put and delete endpoints
+ * @param {object=} data - optional data object, if needed for put and post endpoints
+ * @param {number=} id - optional resource ID, if needed for get, put and delete endpoints
+ * @param {object=} params - optional parameters
  * @returns {object} response 
  */
-export default async function nb_call(clientConfig, method, endpoint, data, id) {
+export default async function nb_call(clientConfig, method, endpoint, data, id, params={}) {
     const { accessToken, nationSlug, siteSlug } = clientConfig;
 
     const fetchOptions = {
@@ -17,14 +18,13 @@ export default async function nb_call(clientConfig, method, endpoint, data, id) 
     };
     if (method === 'POST' || method === 'PUT') { fetchOptions.body = JSON.stringify(data); }
 
+    const queryParams = new URLSearchParams({ ...params, access_token: accessToken });
     const baseUrl = `https://${nationSlug}.nationbuilder.com`
-    const url = baseUrl + endpoint.replace(':site_slug', siteSlug).replace(':id', id) + `?access_token=${accessToken}`;
-
-    console.log('fetchOptions: ', fetchOptions)     //debug
-    console.log('url: ', url)                       //debug
+    const url = baseUrl + endpoint.replace(':site_slug', siteSlug).replace(':id', id) + `?${queryParams}`;
 
     try {
         const response = await fetch(url, fetchOptions);
+        // console.log('nb_call response: ', response)     //debug
         return response;
     } catch (err) {
         if (err) {
